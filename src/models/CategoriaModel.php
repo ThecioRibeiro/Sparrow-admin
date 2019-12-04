745fd49c4d354dfc7b91a3792df7db56ee03e380
ThecioRibeiro <55919924+ThecioRibeiro@users.noreply.github.com>
2019-12-03 21:51:46 -0300

    03-12-2019

--- src/models/CategoriaModel.php
+++ src/models/CategoriaModel.php
@@ -17,4 +17,42 @@ class CategoriaModel{
     }
     
   }
+  public function incluir($dados){
+    var_dump($dados);
+    $conexao = Database::getConection();
+    $nome = $dados['txtNomeCategoria'];
+    $novo = $conexao->prepare("INSERT INTO categorias (nome) VALUES(?)");
+    $novo->bind_param('s', $nome);
+    $novo->execute();
+
+    if ($novo->affected_rows > 0) {
+      $id = mysqli_stmt_insert_id($novo);
+      return $id;
+    }else{
+      return "Erro ao gravar dados";
+    }
+    
+
+    
+  }
+  public function atualizar(){
+    print_r('funcao incluir');
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $categoria = new CategoriaModel;
+  var_dump($_POST);
+
+  $acao  = isset($_POST['acao']);
+
+  if ($acao == "insert"){
+      print_r("entrou insert");
+      $categoria->incluir($_POST);
+  } if ($acao == "update"){
+      print_r("entrou update");
+      $categoria->atualizar();
+  }
+  
+  
 }
